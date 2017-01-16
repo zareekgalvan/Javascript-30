900def83648f7a61577c375c16e995a29fa7f484
Armando Galván <armandocgv@gmail.com>
2017-01-16 08:44:38 -0600

    Video now pauses with button and button changes between play and pause icon

---
+++ Video Player/scripts.js
@@ -0,0 +1,23 @@
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+function togglePlay() {
+	video[video.paused ? 'play' : 'pause']();
+}
+
+function updateButton() {
+	const icon = this.paused ? '►' : '❚ ❚';
+	toggle.textContent = icon;
+}
+
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
