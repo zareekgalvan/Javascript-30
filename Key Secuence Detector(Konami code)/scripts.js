a72a849ea05e6b021835c0025de78fe136949ddb
Armando Galván <armandocgv@gmail.com>
2017-01-17 10:18:09 -0600

    App now displays a secret when the correct code is pressed

---
+++ Key Secuence Detector(Konami code)/scripts.js
@@ -0,0 +1,13 @@
+ const pressed = [];
+ const secretCode = 'zareek'
+
+ window.addEventListener('keyup', (e) => {
+
+ 	pressed.push(e.key);
+ 	pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+ 	if (pressed.join('').includes(secretCode)) {
+ 		console.log("SECRET!!!")
+ 		cornify_add();
+ 	}
+
+ });
